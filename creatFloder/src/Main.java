2702f1a1729b82100dda8558ec238d6e0ae80fbe
olency <2466162253@qq.com>
2017-01-23 23:01:38 +0800

    后台
    后台程序已完成，等前台完成既可

--- creatFloder/src/Main.java
+++ creatFloder/src/Main.java
@@ -1,12 +1,27 @@
-public class Main {
+import Service.GetPath;
+import ServiceImpl.GetPathImpl;
+
+import javax.swing.*;
+
+public class Main extends JFrame {
+
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        //获取选择地址
-        String path="";
-        //获取svn文件地址
-        String svnPat="";
-        //生成桌面文件夹
+//        String targetUrl = "D:\\workspace\\bdcdj_cs\\server\\target\\estateplat-server-V1.1.0-SNAPSHOT-20170120100307";
+//        String[] svnUrl = {"/estateplat/branches/server/cs/server/src/main/java/cn/gtmap/estateplat/server/core/mapper/DjsjFwMapper.java", "/estateplat/branches/server/cs/server/src/main/java/cn/gtmap/estateplat/server/core/service/impl/DjsjFwServiceImpl.java ", "/estateplat/branches/server/cs/server/src/main/resources/conf/mybatis/DjsjFw.xml" };
+//        GetPath getPath = new GetPathImpl();
+//        getPath.copyFileAndFloder(svnUrl, targetUrl);
+        creatFrame();
+    }
+
+    public static void creatFrame() {
+        JLabel targetLabel = new JLabel("本地target地址：");
+        JLabel svnLable = new JLabel("svn地址：");
+        JTextField url = new JTextField(12);
+        JTextArea svnUrl = new JTextArea(20, 20);
+        JButton sure = new JButton("拷贝文件到桌面");
 
     }
+
+
 }
--- creatFloder/src/ServiceImpl/GetPathImpl.java
+++ creatFloder/src/ServiceImpl/GetPathImpl.java
@@ -9,6 +9,7 @@ import javax.swing.filechooser.FileSystemView;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 
 /**
@@ -18,13 +19,28 @@ public class GetPathImpl implements GetPath {
 
     @Override
     public void copyFileAndFloder(String[] paths, String realpath) {
-        if (paths != null && paths.length > 0) {
-            for (String path : paths) {
+        String targetPath = "";
+        String[] marks = {"cn", "conf", "static", "WEB-INF" };
+        if(paths != null && paths.length > 0) {
+            for(String path : paths) {
                 //对path的处理
                 path = path.replace(".java", ".class");
-                //对realpath的处理
+                for(String mark : marks) {
+                    if(path.contains(mark)) {
+                        path = path.substring(path.indexOf(mark) - 1);
+                        if(path.contains(mark)) {
+                            if(mark.equals("cn") || mark.equals("conf"))
+                                targetPath = realpath + "/WEB-INF/classes" + path;
+                            else
+                                targetPath = realpath + path;
+                            break;
+                        }
+                    }
+                }
+                String deskpath = targetPath.substring(targetPath.indexOf("target") + "target".length());
 
-                creatFile(realpath, path);
+                //对realpath的处理
+                creatFile(targetPath, deskpath);
             }
         }
     }
@@ -34,13 +50,15 @@ public class GetPathImpl implements GetPath {
         //桌面地址
         String desktop = FileSystemView.getFileSystemView().getHomeDirectory().getAbsolutePath();
         //创建空文件
-        File file = new File(desktop + "/" + path);
+
+        CreateOutFile(desktop + path);
+        File file = new File(desktop + path);
         File sourceFile = new File(realpath);
         try {
             FileInputStream inputStream = new FileInputStream(sourceFile);
             FileOutputStream outputStream = new FileOutputStream(file);
             byte[] bytes = new byte[1024];
-            while (inputStream.read(bytes) != -1) {
+            while(inputStream.read(bytes) != -1) {
                 outputStream.write(bytes);
             }
             inputStream.close();
@@ -49,6 +67,39 @@ public class GetPathImpl implements GetPath {
             e.printStackTrace();
         }
 
+    }
+
+    public void CreateOutFile(String destFileName) {
+        File file = new File(destFileName);
+        if(file.exists()) {
+            System.out.println("创建单个文件" + destFileName + "失败，目标文件已存在！");
+            return;
+        }
+        if(destFileName.endsWith(File.separator)) {
+            System.out.println("创建单个文件" + destFileName + "失败，目标不能是目录！");
+            return;
+        }
+        if(!file.getParentFile().exists()) {
+            System.out.println("目标文件所在路径不存在，准备创建。。。");
+            if(!file.getParentFile().mkdirs()) {
+                System.out.println("创建目录文件所在的目录失败！");
+                return;
+            }
+        }
+        // 创建目标文件
+        try {
+            if(file.createNewFile()) {
+                System.out.println("创建单个文件" + destFileName + "成功！");
+                return;
+            } else {
+                System.out.println("创建单个文件" + destFileName + "失败！");
+                return;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("创建单个文件" + destFileName + "失败！");
+            return;
+        }
     }
 
 }
